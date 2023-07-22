ba87645de088ed1c0ab0922d1a5498dad0e7ae03
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-07-22 12:09:27 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+const container = document.getElementById('container');
+const squares = 800;
+
+for (let i = 0; i < squares; i++) {
+  const innerDiv = document.createElement('div');
+  innerDiv.className = "square";
+  container.appendChild(innerDiv);
+
+  innerDiv.addEventListener('mouseover', () => {
+    innerDiv.style.animation = "changing 1s linear forwards";
+  });
+
+  innerDiv.addEventListener('mouseout', () => {
+    innerDiv.style.animation = "";
+  });
+}
